7970f26bb5a435ccf6f07c74d62ceb8d2db37259
Adam Jelinek <ajelinek@gmail.com>
2023-10-13 20:54:05 -0500

    initial creation

---
+++ src/constants/quotes.js
@@ -0,0 +1,215 @@
+export const quotes =
+  [
+    {
+      quote: "You will never change your life until you change something you do daily. The secret of your success is found in your daily routine.",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "Success is not a destination; it's a journey. The doing is often more important than the outcome.",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "To advance in your career, you need to grow in your skills",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "People are anxious to imporove their circumstances but are unwilling to improve themselves; they therefore remain bound.",
+      author: "James Allen"
+    },
+    {
+      quote: "A time comes when you need to stop waiting for the man you want to become and start being the man you want to be",
+      author: "Bruce Springsteen"
+    },
+    {
+      quote: "The longer you wait to do something you should do now, the greater the odds that you will never actually do it.",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "If you are clear with what you want, the world responds with clarity.",
+      author: "Loretta Staples"
+    },
+    {
+      quote: "No one can produce great things who is not thoroughly sincere in dealing with himself.",
+      author: "James Russell Lowell"
+    },
+    {
+      quote: "If you want your life to imporove, you must imporove yourself.",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "The only guarantee for failure is to stop trying.",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "You're more likely to act yourself into feeling than feel yourself into action.",
+      author: "Jerome Bruner"
+    },
+    {
+      quote: "The first step toward change is awareness. The second step is acceptance.",
+      author: "Nathaniel Branden"
+    },
+    {
+      quote: "The greatest mistake you can make in life is to be continually fearing you will make one.",
+      author: "Elbert Hubbard"
+    },
+    {
+      quote: "Almost everything you do will seem insignificant, but it is important that you do it.",
+      author: "Mahatma Gandhi"
+    },
+    {
+      quote: "The only person you are destined to become is the person you decide to be.",
+      author: "Ralph Waldo Emerson"
+    },
+    {
+      quote: "Almost every man wastes part of his life in attempts to display qualities which he does not possess.",
+      author: "Samuel Johnson"
+    },
+    {
+      quote: "It's not what you look at that matters, it's what you see.",
+      author: "Henry David Thoreau"
+    },
+    {
+      quote: "It's impossible to learn that which you think you already know.",
+      author: "Epictetus"
+    },
+    {
+      quote: "It's impossible to consistently behave in a manner inconsistent with how we see ourselves.",
+      author: "Zig Ziglar"
+    },
+    {
+      quote: "No factor is more important in people's psychological development and motivation than the self-image they have of themselves.",
+      author: "Nathaniel Branden"
+    },
+    {
+      quote: "When you know what you want, and you want it badly enough, you'll find a way to get it.",
+      author: "Jim Rohn"
+    },
+    {
+      quote: "When a man is pushed, tormented, defeated, he has a chance to learn something.",
+      author: "Ralph Waldo Emerson"
+    },
+    {
+      quote: "When a man has put a limit on what he will do, he has put a limit on what he can do.",
+      author: "Charles M. Schwab"
+    },
+    {
+      quote: "The only way to discover the limits of the possible is to go beyond them into the impossible.",
+      author: "Arthur C. Clarke"
+    },
+    {
+      quote: "To reach your potential, you must know where you want to go and where you currently are.",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "Growth is the great separator between those who succeed and those who do not. When I see a person beginning to separate themselves from the pack, it's almost always due to personal growth.",
+      author: "John C. Maxwell"
+    },
+    {
+      quote: "A compelling vision can light your way in even the darkest moments.",
+      author: "Ken Blanchard and Jesse Stoner"
+    },
+    {
+      quote: "Vision is the lifeblood of an organization, a living dynamic force that creates a future that never existed before.",
+      author: "Ken Blanchard and Jesse Stoner"
+    },
+    {
+      quote: "No deal is better than a bad deal.",
+      author: "Chris Voss"
+    },
+    {
+      quote: "The most important words in any negotiation are 'What am I missing?'",
+      author: "Chris Voss"
+    },
+    {
+      quote: "Negotiation is not an act of battle; it's a process of discovery.",
+      author: "Chris Voss"
+    },
+    {
+      quote: "Trust is the key not only to business success, but also to personal happiness.",
+      author: "Stephen M.R. Covey"
+    },
+    {
+      quote: "Feedback is the breakfast of champions.",
+      author: "Ken Blanchard and Spencer Johnson"
+    },
+    {
+      quote: "The best minute I spend is the one I invest in people.",
+      author: "Ken Blanchard and Spencer Johnson"
+    },
+    {
+      quote: "If someone is going down the wrong road, he doesn't need motivation to speed him up. What he needs is education to turn him around.",
+      author: "Jim Rohn"
+    },
+    {
+      quote: "The best way to predict the future is to create it.",
+      author: "Peter Drucker"
+    },
+    {
+      quote: "The most important thing in communication is hearing what isn't said.",
+      author: "Peter Drucker"
+    },
+    {
+      quote: "All truths are easy to understand once they are discovered; the point is to discover them.",
+      author: "Galileo Galilei"
+    },
+    {
+      quote: "Successful people do what unsuccessful people are not willing to do. Don't wish it were easier; wish you were better.",
+      author: "Jim Rohn"
+    },
+    {
+      quote: "Successful people ask better questions, and as a result, they get better answers.",
+      author: "Tony Robbins"
+    },
+    {
+      quote: "The wise man questions himself, the fool others.",
+      author: "Henri Arnold"
+    },
+    {
+      quote: "Have the courage to follow your heart and intuition. They somehow already know what you truly want to become.",
+      author: "Steve Jobs"
+    },
+    {
+      quote: "Have patience. All things are difficult before they become easy.",
+      author: "Saadi"
+    },
+    {
+      quote: "In time, you will discover that all failing really does is give you the opportunity to learn something new.",
+      author: "Gary John Bishop"
+    },
+    {
+      quote: "Hard work is really the accumulation of easy things you didn't do when you should have.",
+      author: "Gary John Bishop"
+    },
+    {
+      quote: "The Great composer does not set to work because he is inspired, but becomes inspired because he is working.",
+      author: "Ernest Newman"
+    },
+    {
+      quote: "Wheather you are a success or failure in life has little to do with your circumstances; it has much more to do with your choices.",
+      author: "Nido Qubein"
+    },
+    {
+      quote: "Don't be a pumpkin",
+      author: "Me *ask me about this one sometime*"
+    },
+    {
+      quote: "You are the same today that you are going to be in five years from now except for two things: the people with whom you associate and the books you read.",
+      author: "Charles Jones"
+    },
+    {
+      quote: "keep company with good men, and you will increase their number.",
+      author: "Rumi"
+    },
+    {
+      quote: "Yesterday I was clever, so I wanted to change the world. Today I am wise, so I am changing myself.",
+      author: "Rumi"
+    },
+    {
+      quote: "Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin.",
+      author: "Mother Teresa"
+    },
+    {
+      quote: "Whatever course you decide upon, there is always someone to tell you that you are wrong.",
+      author: "Ralph Waldo Emerson"
+    }
+  ]
