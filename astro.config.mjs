26de293f9736101aebffd6356d215d2c309a8f64
Adam Jelinek <ajelinek@gmail.com>
2023-10-15 21:35:03 -0500

    added read count

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config'
 
 import mdx from "@astrojs/mdx"
+import { remarkReadingTime } from './plugins/remark-reading-time.mjs'
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [ mdx() ],
+  markdown: {
+    remarkPlugins: [ remarkReadingTime ],
+  },
   site: 'https://ajelinek.github.io',
   base: '/about-me',
 })
